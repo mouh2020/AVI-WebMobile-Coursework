7560e36d67ca56d658dc6c444dad4ff166df604d
mouh2020 <badimohammed2019@gmail.com>
2023-11-28 14:03:01 +0100

    solve first exercice

--- Programmation procédurale/exercices/Les strucutres de controle/exercice01.c
+++ Programmation procédurale/exercices/Les strucutres de controle/exercice01.c
@@ -5,16 +5,16 @@ int main()
 {
     int a;
     printf("Veuillez entrer un nombre : ");
-    scanf("%i",&a);
-    if (a = 0) {
-        printf("Votre nombre est null");
-    }
-    else if (a > 0 ) {
-        printf("Votre nombre est positif");
-    }
+    scanf("%d",&a);
+    if (a > 0) {
+        printf("Votre nombre est positif\n");
+        } 
     else if (a < 0 ) {
-        printf("Votre nombre est negatif");
-    };
+        printf("Votre nombre est negatif\n");
+        } 
+    else {
+        printf("Votre nombre est null\n");
+        }
 
     return 0;
 }

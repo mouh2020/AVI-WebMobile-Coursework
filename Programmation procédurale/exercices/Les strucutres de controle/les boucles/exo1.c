880f47f4cc5ad4921a14dc1585e53371913b984f
mouh2020 <badimohammed2019@gmail.com>
2024-01-16 16:20:55 +0100

    les boucles et organiser  les cours

---
+++ Programmation procédurale/exercices/Les strucutres de controle/les boucles/exo1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define som 10
+
+int main()
+{
+    int a;
+    printf("Veuillez saisir un nombre : ");
+    scanf("%d",&a);
+    do while (a)
+    {
+        /* code */
+    }
+    
+
+}

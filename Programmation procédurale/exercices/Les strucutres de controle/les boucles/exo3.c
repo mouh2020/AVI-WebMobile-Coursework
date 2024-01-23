3d65fa38d4951c194107a67d7192e092bfd52272
mouh2020 <badimohammed2019@gmail.com>
2024-01-23 16:07:11 +0100

    correction exo3

--- Programmation procédurale/exercices/Les strucutres de controle/les boucles/exo3.c
+++ Programmation procédurale/exercices/Les strucutres de controle/les boucles/exo3.c
@@ -3,15 +3,25 @@
 
 int main() {
     int som_pos=0,pro_neg=1,last_num = 1 ;
-    do {
-        printf("Veuillez entrer un nombre SVP : ");
+    while (last_num !=0)
+    {   
+        printf("Veuillez entrer un nombre :");
         scanf("%d",&last_num);
-        if (last_num<0){
-            pro_neg*=last_num;
+        if ((last_num==0)&&(pro_neg==1)&&(som_pos==0)){
+            pro_neg=0;
+            printf("Votre liste est vide.\n");
         }
-        if (last_num>0){
-            som_pos+=last_num;
-        }
-    } while (last_num !=0);
-    printf("Somme : %d\nProduit : %d\n",som_pos,pro_neg);
+        else {
+            if (last_num>0){
+                som_pos+=last_num;
+            }
+            else if (last_num<0){
+                pro_neg*=last_num;
+            }
+            else {
+                printf("Somme : %d\nProduit : %d\n",som_pos,pro_neg);
+            }
+            }
+    }
+    
 }

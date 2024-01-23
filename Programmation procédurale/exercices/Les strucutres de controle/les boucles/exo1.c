8ff1c38b208bd11bca7cdde9bbf0976d716457ae
mouh2020 <badimohammed2019@gmail.com>
2024-01-23 13:51:37 +0100

    correction d'exo01 en C

--- Programmation procédurale/exercices/Les strucutres de controle/les boucles/exo1.c
+++ Programmation procédurale/exercices/Les strucutres de controle/les boucles/exo1.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define som 10
 
 int main()
 {
-    int a;
+    int a,counter;
+    counter = 0 ;
     printf("Veuillez saisir un nombre : ");
     scanf("%d",&a);
-    do while (a)
+    if (a < 0) 
     {
-        /* code */
+        printf("Veullez entrer un nombre positif.\n");
     }
+
+    else 
+    {
+        for (int i=1;i<=a;i++)
+        {
+            counter=counter+i;
+        
+        }
+        printf("La somme est : %d\n",counter);
+    }
+    
+    return 0;
     
 
 }

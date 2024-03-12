793dd0df83ad6ae1787f97d6a35a65f9c7278978
mouh2020 <badimohammed2019@gmail.com>
2024-03-12 09:19:27 +0100

    exo3 correction

--- S2/java/travaux_pratiques/Exos.java
+++ S2/java/travaux_pratiques/Exos.java
@@ -3,20 +3,32 @@ package S2.java.travaux_pratiques;
 public class Exos {
 
     public static void main(String[] args) {
-        //Exo01
+        // Exo01
 
         int a = 15;
-        double b = 17.5;
-        System.out.print("\nLa valeur de a : " + a + "\n"+
+        int b = 17;
+        System.out.println("\nLa valeur de a : " + a + "\n"+
                          "La valeur de b : " + b + "\n");
 
         // Exo2
-        double c;
-        c = b;
-        b = a;
 
-        System.out.print("\nLa valeur de a : " + c + "\n"+
+        int c;
+        c = a;
+        a = b;
+        b = c;
+
+        System.out.println("La valeur de a : " + a + "\n"+
                          "La valeur de b : " + b + "\n\n");
+
+        // Exo3
+
+        int nombre = 5, diviseur=2;
+
+        System.out.println("Le quotien de la division est : " + (nombre/diviseur) + "\n" +
+                           "Le reste de la diision est : " + (nombre%diviseur) + "\n\n");
+
+
+
     }
     
 }

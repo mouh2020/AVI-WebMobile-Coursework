03efc26f51e5d24910eea8affdd067bb5ca7dc9f
mouh2020 <badimohammed2019@gmail.com>
2024-03-12 09:48:21 +0100

    exo3 avec scanner

--- S2/java/travaux_pratiques/Exos.java
+++ S2/java/travaux_pratiques/Exos.java
@@ -1,4 +1,5 @@
 package S2.java.travaux_pratiques;
+import java.util.Scanner;
 
 public class Exos {
 
@@ -27,7 +28,22 @@ public class Exos {
         System.out.println("Le quotien de la division est : " + (nombre/diviseur) + "\n" +
                            "Le reste de la diision est : " + (nombre%diviseur) + "\n\n");
 
+        // Exo3 avec Scanner
 
+        Scanner scanner = new Scanner(System.in);
+        int nbr,div;
+
+        System.out.print("Veuillez entrer un nombre entier : ");
+
+        nbr = scanner.nextInt();
+
+        System.out.print("Veuillez entrer le diviseur: ");
+
+        div = scanner.nextInt();
+
+        System.out.println("\nLe quotien de la division est : " + (nbr/div) + "\n" +
+                           "Le reste de la diision est : " + (nbr%div) + "\n\n");
+        scanner.close();
 
     }
     

7df966d2d401432132ef121d11fec55553f297a0
badimohammed2019@gmail.com <badimohammed2019@gmail.com>
2024-10-22 15:17:45 +0100

    parent calsses

--- S3/JEE/exo02/src/Main.java
+++ S3/JEE/exo02/src/Main.java
@@ -7,8 +7,12 @@ public class Main {
         ville03.setNomVile("Constantine");
         ville03.setNomPays("Algerie");
         ville03.setNbrHabitants(20000);
-        System.out.println(ville02);
-        System.out.println(ville03);
+        System.out.println(ville02.afficher());
+        System.out.println(ville03.afficher());
+        VilleAgricole mavilleagri = new VilleAgricole("Annaba","Algerie",15000,"Dattes",15000);
+        VilleIndustrielle indville = new VilleIndustrielle("Hassi Messaoud","Algerie",17000,"Pétrole",5);
+        System.out.println(mavilleagri.afficher());
+        System.out.println(indville.afficher());
 
     }
 }
--- S3/JEE/exo02/src/Ville.java
+++ S3/JEE/exo02/src/Ville.java
@@ -32,8 +32,7 @@ public class Ville {
         return nbrHabitants;
     }
 
-    @Override
-    public String toString() {
+    public String afficher() {
         return "Ville{" +
                 "nomVille='" + nomVille + '\'' +
                 ", nomPays='" + nomPays + '\'' +
@@ -45,4 +44,48 @@ public class Ville {
         this.nbrHabitants = nbrHabitants;
     }
 
+}
+
+class VilleAgricole extends Ville {
+    String produitAgricole;
+    double soupTerres;
+
+    public VilleAgricole(String pNom, String pPays, int pNbre, String produitAgricole, double soupTerres) {
+        super(pNom, pPays, pNbre);
+        this.produitAgricole = produitAgricole;
+        this.soupTerres = soupTerres;
+    }
+
+    @Override
+    public String afficher() {
+        return "VilleAgricole{" +
+                "produitAgricole='" + produitAgricole + '\'' +
+                ", soupTerres=" + soupTerres +
+                ", nomVille='" + nomVille + '\'' +
+                ", nomPays='" + nomPays + '\'' +
+                ", nbrHabitants=" + nbrHabitants +
+                '}';
+    }
+}
+
+class VilleIndustrielle extends Ville {
+    String industrie;
+    int nbUsines;
+
+    public VilleIndustrielle(String pNom, String pPays, int pNbre, String industrie, int nbUsines) {
+        super(pNom, pPays, pNbre);
+        this.industrie = industrie;
+        this.nbUsines = nbUsines;
+    }
+
+    @Override
+    public String afficher() {
+        return "VilleIndustrielle{" +
+                "industrie='" + industrie + '\'' +
+                ", nbUsines=" + nbUsines +
+                ", nbrHabitants=" + nbrHabitants +
+                ", nomPays='" + nomPays + '\'' +
+                ", nomVille='" + nomVille + '\'' +
+                '}';
+    }
 }
